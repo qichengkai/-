82a4cd30782674991245a2d753e924e490ada87d
qichengkai <2586651867@qq.com>
2017-07-15 18:33:35 +0800

    Add files via upload

---
+++ oldbate-game-0.0.0.0.0.1-17w00b.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+a = 200 #身上的钱进行设定
+b = 4500 #银行卡里的钱进行设定
+juexin = 100 #决心进行设定
+print "你现在身上有：",a,"元，银行卡里有：",b,"元。"
+import os
+os.system('pause')
+c = 1 #时间设置
+print "现在是第",c,"天。"
+import os
+os.system('pause')
+print "你想到有朝一日你会变成土豪，这使你充满的决心，想要攒钱，果不其然，你的决心溢了出来，从地上捡到了100块。但是你的银行卡最多只能放4500元，所以只能放自己身上。"
+import os
+os.system('pause')
+juexin = juexin+50 #决心增加
+a = a+100 #钱数增加
+print "上午已经过去了，下午就来了，你走进了银行，准备升级你的银行卡，需要50点决心值，你有",juexin,"点决心值。"
+import os
+os.system('pause')
+juexin = juexin-50
+print "很好，你的银行卡的容量提升至了10000元储存量。"
+import os
+os.system('pause')
+b = b+a
+a = 0
+print "你现在身上有：",a,"元，银行卡里有：",b,"元。今天已经过去了，你的决心为：",juexin
+import os
+os.system('pause')
+print "您玩的不错！"
+import os
+os.system('pause')
+print "游戏已经结束了，您可以再来一局！"
+import os
+os.system('pause')
